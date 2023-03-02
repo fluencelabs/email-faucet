a4835c7afa664a610f30ebea71613147d3d6aeec
Elshan Dzhafarov <raiman.elshan@gmail.com>
2023-03-02 20:32:44 +0400

    fix: add ts ignore (#10)
    * feat: add countly
    
    * fix: add ts ignore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+// @ts-ignore
 import Countly from "countly-sdk-web";
 import { useEffect } from "react";
 
