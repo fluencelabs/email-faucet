4808a7eba08623e7975f2b60f300c34b57d3a3b8
elshan_eth <raiman.elshan@gmail.com>
2023-06-28 00:19:59 +0400

    feat: add multi networks - kras, stage, testnet

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,4 @@
+import { ChainNetwork } from "@fluencelabs/deal-aurora/dist/client/config";
 import { Level } from "level";
 import path from "path";
 
@@ -6,13 +7,20 @@ const db = new Level<string, number>(
   path.join(process.env.FAUCET_DATA_DIR!, "users.db")
 );
 
-async function setLastTimestampToUser(email: string, timestamp: number) {
-  await db.put(email, timestamp);
+async function setLastTimestampToUser(
+  email: string,
+  network: ChainNetwork,
+  timestamp: number
+) {
+  await db.put(`${email}-${network}}`, timestamp);
 }
 
-async function getLastTimeByUser(email: string): Promise<number> {
+async function getLastTimeByUser(
+  email: string,
+  network: ChainNetwork
+): Promise<number> {
   try {
-    return Number(await db.get(email));
+    return Number(await db.get(`${email}-${network}}`));
   } catch (err: any) {
     if (err && err.code === "LEVEL_NOT_FOUND") {
       return 0;
--- src/web3.ts
+++ src/web3.ts
@@ -1,18 +1,14 @@
-import { ethers } from "ethers";
-import { Interface } from "ethers/lib/utils";
+import { Interface, ethers } from "ethers";
 
-const FAUCET_ADDRESS = process.env.FAUCET_CONTRACT_ADDRESS!;
-const provider = new ethers.providers.JsonRpcProvider(
-  process.env.FAUCET_CHAIN_RPC_URL
-);
+const provider = new ethers.JsonRpcProvider(process.env.FAUCET_CHAIN_RPC_URL);
 const wallet = new ethers.Wallet(process.env.FAUCET_PRIVATE_KEY!, provider);
 
 const faucetAbi = new Interface([
   "function usdToken() view returns (address)",
   "function fluenceToken() view returns (address)",
+  "function multicall(bytes[] calldata data) returns (bytes[] memory results)",
   "function sendUSD(address addr, uint256 value)",
   "function sendFLT(address addr, uint256 value)",
-  "function lastTransactionTimestamp(address addr) view returns (uint256)",
 ]);
 
-export { provider, wallet, faucetAbi, FAUCET_ADDRESS };
+export { provider, wallet, faucetAbi };
